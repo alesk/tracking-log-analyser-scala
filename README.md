49fb7a3e49736c2b46447004b2ec3ba83d9a1250
Aleš Kotnik <ales.kotnik@gmail.com>
2014-02-15 23:13:27 +0100

    Add parsing of actual json log data.

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 To run analyzier, type:
 
-    source env
-    sbt run
+    source env && sbt
+    run ../tracking-logs-analyzer/var/spool/tracking-logs/2014-02-11_11-40_akotnik2.events
+
 
 Using [ScalaCheck][1] for testing.
 
--- src/main/scala/app.scala
+++ src/main/scala/app.scala
@@ -1,5 +1,19 @@
-object Hi {
+import scala.util.parsing.json._
+import scala.io.Source
+
+object App {
   def main(args: Array[String]) {
-    println("Hello world")
-  }
+    val fileName = args(0)
+
+    // each JSON record is in one line
+    lazy val parsed = for {
+      raw <- Source.fromFile(fileName).getLines()
+      parsed <- JSON.parseFull(raw) 
+    } yield parsed
+
+    // prints each log record
+    while(parsed.hasNext)
+      println(parsed.next)
+
+  }  
 }
